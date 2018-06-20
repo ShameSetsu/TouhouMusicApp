04c5775f881ca232f41f688b4dda7d4273ba405e
ShameSetsu <shameimaru.setsuri@gmail.com>
2018-06-20 23:08:52 +0200

    add progression bar

--- src/components/current-track-footer/current-track-footer.ts
+++ src/components/current-track-footer/current-track-footer.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from "@angular/core";
 import { MusicPlayer } from "../../services/musicPlayer.service";
 import { AlbumTrackOutDto } from "../../models/trackOutDto";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
     selector: 'current-track-footer',
@@ -11,6 +12,10 @@ export class CurrentTrackFooter {
     track: AlbumTrackOutDto;
     playing: boolean;
     @Output() open: EventEmitter<boolean> = new EventEmitter<boolean>();
+    trackDuration: number = 0;
+    currentTimer = 0;
+    timerSubscription: Subscription;
+    tmpTimer;
 
     constructor(private musicPlayer: MusicPlayer) {
         this.musicPlayer.trackPlaying.subscribe((event: { playing: true, track: AlbumTrackOutDto }) => {
@@ -18,11 +23,17 @@ export class CurrentTrackFooter {
                 this.open.emit(true);
                 this.track = event.track;
                 this.playing = event.playing;
+                this.trackDuration = event.track.duration * 1000;
+                console.log('this.trackDuration', this.trackDuration);
             } else {
                 this.open.emit(false);
                 this.track = null;
                 this.playing = false;
             }
+        });
+        this.timerSubscription = this.musicPlayer.trackTimer.subscribe(timer=>{
+            console.log('firstSub', timer);
+            this.currentTimer = timer;
         });
     }
 
@@ -36,5 +47,24 @@ export class CurrentTrackFooter {
 
     nextTrack() {
         this.musicPlayer.next();
+    }
+
+    durationChange(value){
+        this.tmpTimer = value;
+    }
+
+    selectTimer(event: Event){
+        event.preventDefault();
+        console.log('selectTimer', event);
+        this.timerSubscription.unsubscribe();
+        setTimeout(() => {
+            console.log('position', this.tmpTimer);
+            this.musicPlayer.setPosition(this.tmpTimer).then(()=>{
+                this.timerSubscription = this.musicPlayer.trackTimer.skip(1).subscribe(timer=>{
+                    console.log('secondSub', timer);
+                    this.currentTimer = timer;
+                });
+            });
+        });
     }
 }
--- src/services/musicPlayer.service.ts
+++ src/services/musicPlayer.service.ts
@@ -4,29 +4,68 @@ import { Media, MediaObject, MEDIA_STATUS } from '@ionic-native/media';
 import { AlbumTrackOutDto } from '../models/trackOutDto';
 import { Settings } from '../settings';
 import { Subscription } from 'rxjs/Subscription';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class MusicPlayer {
     playlist: Array<AlbumTrackOutDto>;
     currentTrack: AlbumTrackOutDto;
     playingTrack: MediaObject;
+    public trackTimer: BehaviorSubject<number> = new BehaviorSubject<number>(0);
     public trackPlaying: EventEmitter<{ playing: boolean, track: AlbumTrackOutDto }> = new EventEmitter<{ playing: boolean, track: AlbumTrackOutDto }>();
     musicStatus: Subscription;
     volume: number = 0.5;
+    position: number;
+    positionSubscription: Subscription;
 
-    constructor(private media: Media) { }
+    constructor(private media: Media) {
+
+    }
 
     setMusicWatcher() {
         this.musicStatus = this.playingTrack.onStatusUpdate.subscribe((status: MEDIA_STATUS) => {
             console.log('playingTrack status', status);
-            if (status == MEDIA_STATUS.STOPPED) {
-                console.log('MEDIA STOPP')
-                if (this.playlist.findIndex(track => this.findTrack(track, this.currentTrack)) + 1 < this.playlist.length) {
-                    this.playTrack(this.playlist[this.playlist.findIndex(track => this.findTrack(track, this.currentTrack)) + 1]);
-                } else {
-                    this.trackPlaying.emit({ playing: false, track: null });
-                }
+            switch (status) {
+                case MEDIA_STATUS.STARTING:
+                    this.position = 0;
+                case MEDIA_STATUS.RUNNING:
+                    if (this.positionSubscription) this.positionSubscription.unsubscribe();
+                    this.positionSubscription = this.trackTimer.defaultIfEmpty().subscribe(newPosition => {
+                        this.playingTrack.getCurrentPosition().then(position => {
+                            setTimeout(() => {
+                                    this.position = position * 1000;
+                                    this.trackTimer.next(this.position);
+                            }, 30);
+                        });
+                    });
+                    break;
+                case MEDIA_STATUS.PAUSED:
+                    this.positionSubscription.unsubscribe();
+                    break;
+                case MEDIA_STATUS.STOPPED:
+                    console.log('MEDIA STOPP');
+                    this.position = 0;
+                    this.trackTimer.next(this.position);
+                    this.positionSubscription.unsubscribe();
+                    if (this.playlist && this.playlist.findIndex(track => this.findTrack(track, this.currentTrack)) + 1 < this.playlist.length) {
+                        this.playTrack(this.playlist[this.playlist.findIndex(track => this.findTrack(track, this.currentTrack)) + 1]);
+                    } else {
+                        this.trackPlaying.emit({ playing: false, track: null });
+                    }
+                    break;
             }
+        })
+    }
+
+    setPosition(value: number): Promise<void> {
+        return new Promise((resolve)=>{
+            this.position = value;
+            this.positionSubscription.unsubscribe();
+            this.trackTimer.next(value);
+            setTimeout(() => {
+                this.playingTrack.seekTo(value);    
+                resolve();
+            }, 35);
         })
     }
 
@@ -52,6 +91,7 @@ export class MusicPlayer {
             console.log('create media', Settings.ApiHost + ':' + Settings.ApiPort + '/files/music/' + track.file + '.' + track.format);
             this.playingTrack = this.media.create(Settings.ApiHost + ':' + Settings.ApiPort + '/files/music/' + track.file + '.' + track.format);
             this.playingTrack.onStatusUpdate.filter(status => status == MEDIA_STATUS.STOPPED).subscribe(() => this.trackPlaying.emit({ playing: false, track: null }));
+            this.setMusicWatcher();
         }
         this.easeIn();
         this.trackPlaying.emit({ playing: true, track: track });
