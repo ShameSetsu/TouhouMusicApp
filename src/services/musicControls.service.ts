66327f931e06495190e22cd9218edcf4f63e06d0
ADETEL99\jbenethuiliere <jbenethuiliere@witekio.com>
2018-06-26 14:27:14 +0200

    found Music control fix (but not for android 8)

--- src/services/musicControls.service.ts
+++ src/services/musicControls.service.ts
@@ -21,14 +21,14 @@ export class MusicControlsService {
                     if (!this.currentTrack || state.track._id != this.currentTrack._id) {
                         this.currentTrack = state.track;
                         console.log('[MUSIC_CONTROL] Create', this.currentTrack);
-                        this.createControl(state.track).then(res=>{
-                            console.log('[MUSIC_CONTROL] created', res);
-                            this.musicControls.listen();
-                            this.subscribeToControl();
-                        });
+                        this.createControl(state.track);
+                        console.log('[MUSIC_CONTROL] created');
+                        this.subscribeToControl();
+                        this.musicControls.listen();
+                        this.musicControls.updateIsPlaying(true);
                     }
                 } else {
-                    if(this.musicControls) {
+                    if (this.musicControls) {
                         this.musicControls.destroy();
                     }
                 }
@@ -36,36 +36,41 @@ export class MusicControlsService {
         });
     }
 
-    createControl(track: AlbumTrackOutDto): Promise<any> {
-        return this.musicControls.create({
-            track: track.title,        // optional, default : ''
-            artist: track.artist.name,                       // optional, default : ''
-            cover: track.albumThumbnail,      // optional, default : nothing
-            // cover can be a local path (use fullpath 'file:///storage/emulated/...', or only 'my_image.jpg' if my_image.jpg is in the www folder of your app)
-            //           or a remote url ('http://...', 'https://...', 'ftp://...')
-            isPlaying: true,                         // optional, default : true
-            dismissable: false,                         // optional, default : false
-
-            // hide previous/next/close buttons:
-            hasPrev: true,      // show previous button, optional, default: true
-            hasNext: true,      // show next button, optional, default: true
-            hasClose: false,       // show close button, optional, default: false
-
-            // iOS only, optional
-            album: track.album,     // optional, default: ''
-            duration: track.duration, // optional, default: 0
-            elapsed: 0, // optional, default: 0
-            // hasSkipForward: false,  // show skip forward button, optional, default: false
-            // hasSkipBackward: false, // show skip backward button, optional, default: false
-            // skipForwardInterval: 15, // display number for skip forward, optional, default: 0
-            // skipBackwardInterval: 15, // display number for skip backward, optional, default: 0
-            // hasScrubbing: false, // enable scrubbing from control center and lockscreen progress bar, optional
-
-            // Android only, optional
-            
-            ticker: '演奏する: ' + track.title, // text displayed in the status bar when the notification (and the ticker) are updated, optional
-            // All icons default to their built-in android equivalents
-            playIcon: 'media_play', // The supplied drawable name, e.g. 'media_play', is the name of a drawable found under android/res/drawable* folders
+    createControl(track: AlbumTrackOutDto) {
+        console.log('control', {
+            track: track.title,
+            artist: track.artist.name,
+            cover: track.albumThumbnail,
+            isPlaying: true,
+            dismissable: false,
+            hasPrev: true,
+            hasNext: true,
+            hasClose: false,
+            album: track.album,
+            duration: track.duration,
+            elapsed: 0,
+            ticker: track.title,
+            playIcon: 'media_play',
+            pauseIcon: 'media_pause',
+            prevIcon: 'media_prev',
+            nextIcon: 'media_next',
+            closeIcon: 'media_close',
+            notificationIcon: 'notification'
+        });
+        this.musicControls.create({
+            track: track.title,
+            artist: track.artist.name,
+            cover: 'http://i0.kym-cdn.com/photos/images/newsfeed/000/823/942/b1d.png', // IMAGE NOT FOUND == NO MUSIC CONTROLS SHOWN !!
+            isPlaying: true,
+            dismissable: false,
+            hasPrev: true,
+            hasNext: true,
+            hasClose: false,
+            album: track.album,
+            duration: track.duration,
+            elapsed: 0,
+            ticker: track.title,
+            playIcon: 'media_play',
             pauseIcon: 'media_pause',
             prevIcon: 'media_prev',
             nextIcon: 'media_next',
@@ -90,21 +95,6 @@ export class MusicControlsService {
                     break;
                 case 'music-controls-play':
                     this.player.play();
-                    break;
-                case 'music-controls-destroy':
-                    // Do something
-                    break;
-
-                // Headset events (Android only)
-                // All media button events are listed below
-                case 'music-controls-media-button':
-                    // Do something
-                    break;
-                case 'music-controls-headset-unplugged':
-                    // Do something
-                    break;
-                case 'music-controls-headset-plugged':
-                    // Do something
                     break;
                 default:
                     break;
